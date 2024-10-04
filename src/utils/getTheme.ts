a10b3c97c64522682b0a3e21b8bf8f08102fdf58
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2024-10-05 05:43:25 +0700

    fix: build issue

--- src/utils/getTheme.ts
+++
@@ -1,8 +0,0 @@
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.js";
-
-const getTailwindConfiguration = () => {
-  return resolveConfig(tailwindConfig).theme;
-};
-
-export const theme = getTailwindConfiguration();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// @ts-expect-error no types
 import path from "path";
+// @ts-expect-error no types
 import * as url from "url";
 
+// @ts-expect-error no types
 const dirName = url.fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
